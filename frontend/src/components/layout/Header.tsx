e398ec419cf9bbfcb230f217280a2f642d240894
Mortymier <sanielmg@gmail.com>
2025-12-21 04:12:27 +0800

    Frontend: add Header component

---
+++ frontend/src/components/layout/Header.tsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+import wildocsai_logo from '../../assets/wildocsai_logo.svg';
+import '../styles/Header.css';
+import '../styles/Buttons.css';
+
+export default function Header()
+{
+    return (
+        <header className="header-container">
+            <Link to="/"> <img src={wildocsai_logo} alt="Wildocs AI Logo"/> </Link>
+            <Link to="/" className="site-name"> 
+                <h1> WILDOCS AI </h1>
+                <h2> AI-Powered SDD Evaluator </h2>
+            </Link>
+            <div className="header-buttons">
+                <Link to="/" className="btn-white"> Login </Link>
+                <Link to="/" className="btn-maroon"> Register </Link>
+            </div>
+        </header>
+    );
+}
