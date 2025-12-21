d027ea0869fa838b23e56b8194624b4ffa9e7e46
Mortymier <sanielmg@gmail.com>
2025-12-21 22:02:44 +0800

    Frontend: add routes for Home and Test pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+{/* Public Pages */}
+import Home from './pages/authentication/Home.tsx';
+import Test from './pages/authentication/Test.tsx';
+
 export default function App()
 {
     return (
         <BrowserRouter>
             <Routes>
-                
+                <Route index element={<Home/>}/>
+                <Route path="/test" element={<Test/>}/>
             </Routes>
         </BrowserRouter>
     );
