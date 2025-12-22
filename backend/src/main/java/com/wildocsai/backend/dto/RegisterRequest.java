e94403125ca17e375b64efb673643d3276f40527
Mortymier <sanielmg@gmail.com>
2025-12-22 22:35:27 +0800

    Backend: add RegisterRequest DTO

---
+++ backend/src/main/java/com/wildocsai/backend/dto/RegisterRequest.java
@@ -0,0 +1,27 @@
+package com.wildocsai.backend.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegisterRequest
+{
+    @NotBlank(message = "First name cannot be blank")
+    private String firstName;
+
+    @NotBlank(message = "Last name cannot be blank")
+    private String lastName;
+
+    @NotBlank(message = "ID number cannot be blank")
+    private String idNum;
+
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Please provide a valid email address")
+    private String email;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+}
