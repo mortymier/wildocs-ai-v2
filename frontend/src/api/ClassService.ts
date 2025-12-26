ab6cfad5fbb827eaab2707b7009873968ae2ea57
Mortymier <sanielmg@gmail.com>
2025-12-27 04:04:39 +0800

    PH-27 Frontend: add API call function for fetching teacher classes

--- frontend/src/api/ClassService.ts
+++ frontend/src/api/ClassService.ts
@@ -28,3 +28,30 @@ export const createClass = async(formData: CreateClassForm, email: string) =>
         throw new Error('Creating class failed. Please try again.');
     }
 };
+
+
+export const getClassesByTeacher = async(email: string) =>
+{
+    try
+    {
+        const response = await axios.get
+        (
+            `http://localhost:8080/api/class/all/teacher`, 
+            { 
+                params: { email }, 
+                withCredentials: true
+            }
+        );
+
+        return response.data;
+    }
+    catch(error: any)
+    {
+        if (error.response?.data)
+        {
+            throw new Error(error.response.data);
+        }
+
+        throw new Error('Fetching classes failed. Please try again.');
+    }
+}
